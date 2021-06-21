db7a97bebb95f3439177eddbe3eb4be55fd39b79
Adam Gagorik <adam.gagorik@gmail.com>
2021-06-20 20:40:21 -0400

    Have settings load database URL from environment variable

--- project/app/config.py
+++ project/app/config.py
@@ -2,14 +2,15 @@ import functools
 import logging
 import os
 
-import pydantic
+from pydantic import AnyUrl, BaseSettings
 
 logger = logging.getLogger('uvicorn')
 
 
-class Settings(pydantic.BaseSettings):
+class Settings(BaseSettings):
     environment: str = os.environ.get('ENVIRONMENT', 'dev')
     testing: bool = os.environ.get('TESTING', 0)
+    database_url: AnyUrl = os.environ.get('DATABASE_URL')
 
 
 @functools.lru_cache()
