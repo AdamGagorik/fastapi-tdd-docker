a41bbd5d24fc22446b79b789a93e4215b4768caf
Adam Gagorik <adam.gagorik@gmail.com>
2021-06-22 21:24:00 -0400

    Add REST endpoint for get summary

--- project/app/api/summaries.py
+++ project/app/api/summaries.py
@@ -4,6 +4,7 @@ REST API endpoints for summary table.
 from fastapi import APIRouter, HTTPException
 
 from app.api import crud
+from app.models.tortoise import SummarySchema
 from app.models.pydantic import SummaryPayloadSchema, SummaryResponseSchema
 
 
@@ -17,3 +18,12 @@ async def create_summary(payload: SummaryPayloadSchema) -> SummaryResponseSchema
     """
     summary_id = await crud.post(payload)
     return SummaryResponseSchema(id=summary_id, url=payload.url)
+
+
+@router.get("/{id}/", response_model=SummarySchema)
+async def read_summary(id: int) -> SummarySchema:
+    """
+    Get a summary entry from database.
+    """
+    summary = await crud.get(id)
+    return summary
