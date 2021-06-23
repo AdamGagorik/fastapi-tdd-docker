9daf369a419d5afaa1906aae3ca72c5001fc0bd8
Adam Gagorik <adam.gagorik@gmail.com>
2021-06-22 21:23:17 -0400

    Add get method for summary

--- project/app/api/crud.py
+++ project/app/api/crud.py
@@ -3,6 +3,7 @@ Create, Read, Update, Delete methods for the database.
 """
 from app.models.pydantic import SummaryPayloadSchema
 from app.models.tortoise import TextSummary
+from typing import Union
 
 
 async def post(payload: SummaryPayloadSchema) -> int:
@@ -18,3 +19,19 @@ async def post(payload: SummaryPayloadSchema) -> int:
     summary = TextSummary(url=payload.url, summary="dummy summary")
     await summary.save()
     return summary.id
+
+
+async def get(id: int) -> Union[dict, None]:
+    """
+    Get existing entry from the database.
+
+    Parameters:
+        id: The ID of the entry to get.
+
+    Returns:
+        The response.
+    """
+    summary = await TextSummary.filter(id=id).first().values()
+    if summary:
+        return summary[0]
+    return None
