82120784f9bb1afdd62564a1dc23490456ff97fc
Adam Gagorik <adam.gagorik@gmail.com>
2021-06-22 21:23:49 -0400

    Add unit test for get summary method

--- project/tests/test_summaries.py
+++ project/tests/test_summaries.py
@@ -7,3 +7,17 @@ def test_create_summary(test_app_with_db):
     response = test_app_with_db.post("/summaries/", json={"url": "https://foo.bar"})
     assert response.status_code == 201, response.json()
     assert response.json()["url"] == "https://foo.bar"
+
+
+def test_read_summary(test_app_with_db):
+    response = test_app_with_db.post("/summaries/", json={"url": "https://foo.bar"})
+    summary_id = response.json()["id"]
+
+    response = test_app_with_db.get(f"/summaries/{summary_id}/")
+    assert response.status_code == 200
+
+    response_dict = response.json()
+    assert response_dict["id"] == summary_id
+    assert response_dict["url"] == "https://foo.bar"
+    assert response_dict["summary"]
+    assert response_dict["created_at"]
