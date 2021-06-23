3d406641a3a0b89385f2eb4857bc1f31e63e5054
Adam Gagorik <adam.gagorik@gmail.com>
2021-06-22 21:25:40 -0400

    Add unit test for invalid post

--- project/tests/test_summaries.py
+++ project/tests/test_summaries.py
@@ -9,6 +9,20 @@ def test_create_summary(test_app_with_db):
     assert response.json()["url"] == "https://foo.bar"
 
 
+def test_create_summaries_invalid_json(test_app_with_db):
+    response = test_app_with_db.post("/summaries/", json={})
+    assert response.status_code == 422
+    assert response.json() == {
+        "detail": [
+            {
+                "loc": ["body", "url"],
+                "msg": "field required",
+                "type": "value_error.missing"
+            }
+        ]
+    }
+
+
 def test_read_summary(test_app_with_db):
     response = test_app_with_db.post("/summaries/", json={"url": "https://foo.bar"})
     summary_id = response.json()["id"]
