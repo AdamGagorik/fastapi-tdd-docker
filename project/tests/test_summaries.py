8cc5158f9fe9966fb252fd454284bb2df7776493
Adam Gagorik <adam.gagorik@gmail.com>
2021-06-26 21:33:21 -0400

    Enforce UPDATE id > 0

--- project/tests/test_summaries.py
+++ project/tests/test_summaries.py
@@ -108,11 +108,29 @@ def test_update_summary(test_app_with_db):
 
 
 def test_update_summary_incorrect_id(test_app_with_db):
+    # cant update a summary that does not exist
     response = test_app_with_db.put(
         "/summaries/999/", json={"url": "https://foo.bar", "summary": "updated!"}
     )
     assert response.status_code == 404
     assert response.json()["detail"] == "Summary not found"
+
+    # cant update summary 0
+    response = test_app_with_db.put(
+        f"/summaries/0/",
+        json={"url": "https://foo.bar", "summary": "updated!"}
+    )
+    assert response.status_code == 422
+    assert response.json() == {
+        "detail": [
+            {
+                "loc": ["path", "id"],
+                "msg": "ensure this value is greater than 0",
+                "type": "value_error.number.not_gt",
+                "ctx": {"limit_value": 0},
+            }
+        ]
+    }
 
 
 def test_update_summary_invalid_json(test_app_with_db):
